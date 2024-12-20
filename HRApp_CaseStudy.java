681bd34ce763883d710e25b5e75665e204e23df6
CharliePoh <pingsengpoh@hotmail.com>
2024-12-20 13:20:42 +0800

    update

--- HRApp_CaseStudy.java
+++ HRApp_CaseStudy.java
@@ -6,9 +6,9 @@ public class HRApp_CaseStudy {
     public static void main(String[] args) {
         System.out.println("HR App Starts");
         
-        employee emp1 = new employee(101, "Ann", 1234.56);
-        employee emp2 = new employee(102, "Bob", 1200.34);
-        employee emp3 = new employee(103, "Ray", 1122.33);
+        Employee emp1 = new Employee(101, "Ann", 1234.56);
+        Employee emp2 = new Employee(102, "Bob", 1200.34);
+        Employee emp3 = new Employee(103, "Ray", 1122.33);
         
         Department dept = new Department("Department Education");
 
@@ -18,9 +18,9 @@ public class HRApp_CaseStudy {
         dept.addEmployee(emp2);
         dept.addEmployee(emp3);
         
-        employee[] emp = dept.getEmployee();
+        Employee[] emp = dept.getEmployee();
         
-        for (employee employees : emp) {
+        for (Employee employees : emp) {
             System.out.println(employees);
         }
         
